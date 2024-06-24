f4c19f4499c2a027544d98aff70341cbc6ef867a
Sabina Zeynalova <117943057+ZSabina88@users.noreply.github.com>
2024-06-24 22:47:52 +0400

    some change

--- back/index.js
+++ back/index.js
@@ -9,6 +9,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+});
+
 //database connection
 mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log("Database connected"))
